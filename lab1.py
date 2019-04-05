ad58f88ad192cfc529e4e7826b66e388ce75babb
Jithan <jthyalan@calpoly.edu>
2019-04-05 10:01:04 -0700

    update

--- lab1.py
+++ lab1.py
@@ -2,8 +2,19 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
-#hi
+   count = 0
+   maximum= 0
+   for i in range(len(int_list)-1):
+      if int_list(i) > int_list(i+1):
+         maximum = int_list(i)
+      # elif int_list(i) <= int_list(i+1):
+      #    continue
+      elif len(int_list) ==0:
+         return None
+      elif int_list == None:
+         raise ValueError
+
+
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
